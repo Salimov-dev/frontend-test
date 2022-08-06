a27c5d643f54549fe52a5b3fea035b82069fa637
Ruslan Salimov <work@salimov-dev.ru>
2022-08-06 18:37:14 +0300

    workers

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    $('.content__workmans').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        dots: true,
+        adaptiveHeight: true,
+        speed: 800,
+        autoplay: false,
+        autoplaySpeed: 2500,
+      });
+
+});
